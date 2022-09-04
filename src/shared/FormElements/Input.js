451b19c0665b8cf2ae3c66e910173147e9a74128
Aditya Prayoga <aditya@brainmatics.com>
2022-09-04 14:41:52 +0700

    [ADD] WIP Create Todo

--- src/shared/FormElements/Input.js
+++ src/shared/FormElements/Input.js
@@ -1,28 +1,84 @@
-import React from "react";
+import React, { useReducer, useEffect } from 'react';
 
 import './Input.css';
 
+const inputReducer = (state, action) => {
+  switch (action.type) {
+    case 'CHANGE':
+      return {
+        ...state,
+        value: action.val,
+        isValid: true
+      };
+    case 'TOUCH': {
+      return {
+        ...state,
+        isTouched: true
+      }
+    }
+    default:
+      return state;
+  }
+};
+
 const Input = props => {
-    const element = props.type === 'input' ? 
-    <input
+  const [inputState, dispatch] = useReducer(inputReducer, {
+    value: '',
+    isTouched: true,
+    isValid: true
+  });
+
+  const { id, onInput } = props;
+  const { value, isValid } = inputState;
+
+  useEffect(() => {
+    onInput(id, value, isValid)
+  }, [id, value, isValid, onInput]);
+
+  const changeHandler = event => {
+    dispatch({
+      type: 'CHANGE',
+      val: event.target.value,
+      validators: props.validators
+    });
+  };
+
+  const touchHandler = () => {
+    dispatch({
+      type: 'TOUCH'
+    });
+  };
+
+  const element =
+    props.element === 'input' ? (
+      <input
         id={props.id}
         type={props.type}
         placeholder={props.placeholder}
-        value={props.value}
-    /> : 
-    <textarea
+        onChange={changeHandler}
+        onBlur={touchHandler}
+        value={inputState.value}
+      />
+    ) : (
+      <textarea
         id={props.id}
-        type={props.type}
-        row={props.rows || 3}
-        value={props.value}
-    />
+        rows={props.rows || 3}
+        onChange={changeHandler}
+        onBlur={touchHandler}
+        value={inputState.value}
+      />
+    );
 
-    return (
-        <div className="form-control">
-            <label htmlFor={props.id}>{props.label}</label>
-            {element}
-        </div>
-    )
-}
+  return (
+    <div
+      className={`form-control ${!inputState.isValid && inputState.isTouched &&
+        'form-control--invalid'}`}
+    >
+      <label htmlFor={props.id}>{props.label}</label>
+      {element}
+      {!inputState.isValid && inputState.isTouched && <p>{props.errorText}</p>}
+    </div>
+  );
+};
 
 export default Input;
--- src/todo/pages/NewTodo.js
+++ src/todo/pages/NewTodo.js
@@ -1,29 +1,86 @@
-import React from "react";
+import React, { useCallback, useReducer } from 'react';
 
-import Input from "../../shared/FormElements/Input";
+import Input from '../../shared/FormElements/Input';
+import Button from '../../shared/UIEelements/Button';
+import './NewTodo.css';
 
-import "./NewTodo.css";
+const formReducer = (state, action) => {
+  switch (action.type) {
+    case 'INPUT_CHANGE':
+      let formIsValid = true;
+      for (const inputId in state.inputs) {
+        if (inputId === action.inputId) {
+          formIsValid = formIsValid && action.isValid;
+        } else {
+          formIsValid = formIsValid && state.inputs[inputId].isValid;
+        }
+      }
+      return {
+        ...state,
+        inputs: {
+          ...state.inputs,
+          [action.inputId]: { value: action.value, isValid: action.isValid }
+        },
+        isValid: formIsValid
+      };
+    default:
+      return state;
+  }
+};
 
-const NewTodo = () => {
-    return (
-        <div>
-            <h2 className="center">Tambah Todo</h2>
-            <form className="todo-form">
-                <Input
-                    id="title"
-                    type="input"
-                    placeholder="input"
-                    label="title"
-                />
-                <Input
-                    id="description"
-                    type="textarea"
-                    placeholder="Description"
-                    label="description"
-                />
-            </form>
-        </div>
-    );
-}
+const NewPlace = () => {
+  const [formState, dispatch] = useReducer(formReducer, {
+    inputs: {
+      title: {
+        value: '',
+        isValid: false
+      },
+      description: {
+        value: '',
+        isValid: false
+      }
+    },
+    isValid: false
+  });
 
-export default NewTodo;
+  const inputHandler = useCallback((id, value, isValid) => {
+    dispatch({
+      type: 'INPUT_CHANGE',
+      value: value,
+      isValid: isValid,
+      inputId: id
+    });
+  }, []);
+
+  const placeSubmitHandler = event => {
+    event.preventDefault();
+    console.log(formState.inputs); // send this to the backend!
+  };
+
+  return (
+    <div className='place-form'>
+        <form onSubmit={placeSubmitHandler}>
+            <Input
+                id="title"
+                element="input"
+                type="text"
+                label="Title"
+                errorText="Please enter a valid title."
+                onInput={inputHandler}
+            />
+            <Input
+                id="description"
+                element="textarea"
+                label="Description"
+                errorText="Please enter a valid description (at least 5 characters)."
+                onInput={inputHandler}
+            />
+            <Button type="submit" disabled={!formState.isValid}>
+                ADD PLACE
+            </Button>
+        </form>
+    </div>
+  );
+};
+
+export default NewPlace;
