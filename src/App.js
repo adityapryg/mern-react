226af2d62b93558e0451578628d6410719d0b836
Aditya Prayoga <aditya@brainmatics.com>
2022-09-04 13:52:38 +0700

    [ADD] Halaman new todo dan button new todo

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {
 } from 'react-router-dom';
 import Todo from './todo/pages/Todo';
 import User from './user/pages/User';
+import NewTodo from './todo/pages/NewTodo';
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
     <Router>
       <Switch>
         <Route path="/" exact component={Todo}/>
+        <Route path="/tambah" exact component={NewTodo}/>
         <Route path="/user" exact component={User}/>
         <Redirect to="/"/>
       </Switch>
---
+++ src/todo/pages/NewTodo.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NewTodo = () => {
+    return <h2>Halaman tambah todo</h2>;
+}
+
+export default NewTodo;
